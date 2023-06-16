bd6a657ca69892f859c07e4203b475d95499ff7a
JunJon09 <shibajunjun14@gmail.com>
2023-06-16 13:58:48 +0900

    6/16_コードのリファクタリング

--- modules/findWord_DTW/allCompareDTW.py
+++ modules/findWord_DTW/allCompareDTW.py
@@ -7,6 +7,7 @@ import time
 def main():
     f = open('word.txt', 'r', encoding='UTF-8')
     data = f.readlines()
+    global wordList
     wordList = [d.replace("\n", "") for d in data]
     
     trainLeftList = []
@@ -52,59 +53,59 @@ def main():
                 distance, dispersion = moveDTW.learning(trainLeftList[j], left, trainRightList[j], right, trainBodyList[j], body)
                 distanceList.append(distance)
                 dispersionList.append(dispersion)
-            indexList = list(enumerate(distanceList))
-            dispersioninedxList = list(enumerate(dispersionList))
-            sorted_list = sorted(indexList, key=lambda x: x[1])
-            dispersionSorted_list = sorted(dispersioninedxList, key=lambda x: x[1])
-            # 結果を表示
-            for k, (index, value) in enumerate(sorted_list, start=1):
-                print(f"{k}: {value} 単語 {wordList[index]}")
-                if wordList[index] == wordList[i]:
-                    if k == 1:
-                        oneAnswerCount += 1
-                    fiveAnswerCount += 1
-                if k == 5:
-                    break
-            print("分散")
-            for k, (index, value) in enumerate(dispersionSorted_list, start=1):
-                print(f"{k}: {value} 単語 {wordList[index]}")
-                if wordList[index] == wordList[i]:
-                    if k == 1:
-                        dispersiononeAnswerCount += 1
-                    dispersionfiveAnswerCount += 1
-                if k == 5:
-                    break
             
+            #順位の並び替え
+            averageSorted_list = sortedRank(distanceList)
+            dispersionSorted_list = sortedRank(dispersionList)
+
+            # 結果を表示
+            oneAnswerCount, fiveAnswerCount = printAnswer(averageSorted_list, i) #平均
+            dispersiononeAnswerCount, dispersionfiveAnswerCount = printAnswer(dispersionSorted_list, i) #分散
             print("本当の答え:", wordList[i])
             print("*"*100)
+        
         oneAnswerList.append(oneAnswerCount)
         fiveAnswerList.append(fiveAnswerCount)
         dispersiononeAnswerList.append(dispersiononeAnswerCount)
         dispersionfiveAnswerList.append(dispersionfiveAnswerCount)
 
-    one_sum = 0
-    five_sum = 0
-    for i, (one, five) in enumerate(zip(oneAnswerList, fiveAnswerList)):
-        one_sum += one
-        five_sum += five
-        print(f"{wordList[i]}の正解数: 1位 {one}/5 {(one/5) *100}% 5位まで {five}/5 {(five/5)*100}%")
-    print(f"全体の正解数: 1位 {one_sum}/100 {one_sum}% 5位まで {five_sum}/100 {five_sum}%")
-
-    #分散
-    print("分散")
-    one_sum = 0
-    five_sum = 0
-    for i, (one, five) in enumerate(zip(dispersiononeAnswerList, dispersionfiveAnswerList)):
-        one_sum += one
-        five_sum += five
-        print(f"{wordList[i]}の正解数: 1位 {one}/5 {(one/5) *100}% 5位まで {five}/5 {(five/5)*100}%")
-    print(f"全体の正解数: 1位 {one_sum}/100 {one_sum}% 5位まで {five_sum}/100 {five_sum}%")
+    #全ての結果を表示
+    printALlAnswer(oneAnswerList, fiveAnswerList)
+    printALlAnswer(dispersiononeAnswerList, dispersionfiveAnswerList)
 
 
 
 def split_data(data):
     train, test = train_test_split(data, test_size=5)
     return train, test
+
+#順位の並び替え
+def sortedRank(distanceList):
+    indexList = list(enumerate(distanceList))
+    sorted_list = sorted(indexList, key=lambda x: x[1])
+    return sorted_list
+
+#一単語の出力結果を表示
+def printAnswer(sorted_list, i):
+    for k, (index, value) in enumerate(sorted_list, start=1):
+        print(f"{k}: {value} 単語 {wordList[index]}")
+        if wordList[index] == wordList[i]:
+            if k == 1:
+                oneAnswerCount += 1
+            fiveAnswerCount += 1
+        if k == 5:
+            break
+    return oneAnswerCount, fiveAnswerCount
+
+#全ての出力結果を表示
+def printALlAnswer(oneList, fiveList):
+    one_sum = 0
+    five_sum = 0
+    for i, (one, five) in enumerate(zip(oneList, fiveList)):
+        one_sum += one
+        five_sum += five
+        print(f"{wordList[i]}の正解数: 1位 {one}/5 {(one/5) *100}% 5位まで {five}/5 {(five/5)*100}%")
+    print(f"全体の正解数: 1位 {one_sum}/100 {one_sum}% 5位まで {five_sum}/100 {five_sum}%")
 
 
 if __name__ == "__main__": 
